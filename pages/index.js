f594a5eeeb90f784401be659be1dc6034bee028e
santiago <sochoav8a@gmail.com>
2023-07-25 14:18:14 -0500

    acá ya funciona el gugul

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,21 @@
 
-import { useSession } from "next-auth/react";
+import { signIn, useSession } from "next-auth/react";
 
 
 export default function Home() {
   const { data: session } = useSession()
   if (!session){
-    return "not logged in";
-  }
-  return (
-    <div className='bg-blue-800 w-screen h-screen flex items-center'>
-      <div className='text-center w-full'>
-        <button className='bg-white p-2 rounded-lg'>click me</button>
+    return (
+      <div className='bg-blue-800 w-screen h-screen flex items-center'>
+        <div className='text-center w-full'>
+          <button onClick={() => signIn()} className='bg-white p-2 rounded-lg'>click me</button>
+        </div>
       </div>
-    </div>
-  )
+    );
+  }
+    return(
+      <div>
+        logged in {session.user.email}
+      </div>
+    );
 }
