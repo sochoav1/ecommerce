01a2147b813181b5ca34555b37ce9d3d906b93db
santiago <sochoav8a@gmail.com>
2023-07-25 13:51:40 -0500

    acá funciona el login

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,12 @@
-import { Inter } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+import { useSession } from "next-auth/react";
+
 
 export default function Home() {
+  const { data: session } = useSession()
+  if (!session){
+    return "not logged in";
+  }
   return (
     <div className='bg-blue-800 w-screen h-screen flex items-center'>
       <div className='text-center w-full'>
